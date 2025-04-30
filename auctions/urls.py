9da5c756fa82f2a54e93a10baee33e7b0c2e5ea2
Wuor Bhang <143351848+WuorBhang@users.noreply.github.com>
2025-04-30 22:33:33 +0300

    Add auctions/urls.py 1746041612136

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import (
-    AuctionListCreateView,
+    AuctionListView,
+    AuctionCreateView,
     AuctionDetailView,
     BidCreateView
 )
 
 urlpatterns = [
-    path('', AuctionListCreateView.as_view(), name='auction-list'),
+    path('', AuctionListView.as_view(), name='auction-list'),
+    path('create/', AuctionCreateView.as_view(), name='auction-create'),
     path('<int:pk>/', AuctionDetailView.as_view(), name='auction-detail'),
     path('<int:pk>/bids/', BidCreateView.as_view(), name='bid-create'),
 ]
