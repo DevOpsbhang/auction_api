7000cd6ef124ce058bbeac80c295e6571afc5f9e
Wuor Bhang <143351848+WuorBhang@users.noreply.github.com>
2025-05-01 09:35:03 +0300

    Add auctions/urls.py 1746081300468

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,12 +3,18 @@ from .views import (
     AuctionListView,
     AuctionCreateView,
     AuctionDetailView,
-    BidCreateView
+    BidCreateView,
+    BidListView,
+    BidDetailView,
+    AuctionEndView
 )
 
 urlpatterns = [
     path('', AuctionListView.as_view(), name='auction-list'),
     path('create/', AuctionCreateView.as_view(), name='auction-create'),
     path('<int:pk>/', AuctionDetailView.as_view(), name='auction-detail'),
-    path('<int:pk>/bids/', BidCreateView.as_view(), name='bid-create'),
+    path('<int:pk>/end/', AuctionEndView.as_view(), name='auction-end'),
+    path('<int:pk>/bids/', BidListView.as_view(), name='bid-list'),
+    path('<int:pk>/bids/create/', BidCreateView.as_view(), name='bid-create'),
+    path('bids/<int:pk>/', BidDetailView.as_view(), name='bid-detail'),
 ]
